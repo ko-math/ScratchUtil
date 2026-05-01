e1c6dbf9a3be5b757c3bc47aafd830d590dfdd7e
ko-math <sorehakiemasu@gmail.com>
2026-05-01 22:11:42 +0900

    Update TurbowarpButton.js

--- functions/GETmethod/TurbowarpButton.js
+++ functions/GETmethod/TurbowarpButton.js
@@ -1,1 +1,21 @@
-
+(() => {
+  if(!(document.querySelector('#SUTurbutton'))){
+    const a = document.createElement('a');
+    a.href = location.href.replace('scratch.mit.edu/projects','turbowarp.org');
+    a.target = '_blank';
+    a.id = 'SUTurbutton';
+    
+    const img = document.createElement('img');
+    img.src = 'https://ko-math.github.io/ScratchUtil/t.png';
+    img.style.width = '50px';
+    img.style.height = '50px';
+    img.style.position = 'relative';
+    img.style.left = '10px';
+    img.style.borderRadius = '15px';
+    
+    a.append(img);
+    
+    const target = document.querySelector('#view .preview .inner :first-child');
+    if (target) target.append(a);
+  }
+})();
