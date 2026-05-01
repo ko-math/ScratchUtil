544ecdda67dfef52635b533b960622703b88696e
ko-math <sorehakiemasu@gmail.com>
2026-05-01 20:30:19 +0900

    Update userID.js

--- functions/GETmethod/userID.js
+++ functions/GETmethod/userID.js
@@ -1,1 +1,15 @@
-
+(async () => {
+  if(!(document.querySelector('#SUuserID'))){
+    const u = location.pathname.split("/")[2];
+    const r = await fetch("https://api.scratch.mit.edu/users/" + u);
+    const d = await r.json();
+    
+    const el = document.querySelector('.header-text .profile-details');
+    
+    const current = document.createElement('span');
+    current.textContent = `UserID: ${d.id}`;
+    current.style.color = "#888";
+    current.id = 'SUuserID';
+    el.append(current);
+  }
+})();
