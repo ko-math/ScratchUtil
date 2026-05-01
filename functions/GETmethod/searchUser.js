c313125e4a7921ad0161b1bb502fb82059e734b9
ko-math <sorehakiemasu@gmail.com>
2026-05-01 22:13:50 +0900

    Update searchUser.js

--- functions/GETmethod/searchUser.js
+++ functions/GETmethod/searchUser.js
@@ -1,1 +1,23 @@
-
+(() => {
+  const tabs = document.querySelector('[role="tablist"]');
+  const input = document.querySelector('#frc-q-1088');
+  const search = input.value;
+  fetch("https://api.scratch.mit.edu/users/" + search)
+  .then(r => r.json())
+  .then(d => {
+    const el = document.createElement('a');
+    el.href = `https://scratch.mit.edu/users/${search}/`;
+    tabs.append(el);
+    const img = document.createElement('img');
+    img.src = d.profile.images["90x90"];
+    img.style.width = "55px";
+    img.style.position = 'relative';
+    img.style.top = '10px';
+    img.style.verticalAlign = "middle";
+    el.append(img);
+  }).catch(() => {
+    const el = document.createElement('p');
+    el.textContent = 'なし';
+    tabs.append(el);
+  });
+})();
