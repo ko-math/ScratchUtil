2dd27cdbbdb3b1e52f7fa6220732be6149cc4e1b
ko-math <sorehakiemasu@gmail.com>
2026-05-02 22:37:15 +0900

    Update function.js

--- functions/function.js
+++ functions/function.js
@@ -1,1 +1,22 @@
+const url = location.href;
+if(url.includes('scratch.mit.edu')){
+    if(url.includes('/users/')){
+        run('GET','userID');
+    }
+    if(url.includes('/projects/')){
+        run('GET','TurbowarpButton');
+    }
+    if(url.includes('/search/')){
+        run('GET','searchUser');
+    }
+} else {
+    alert('Scratchで開いてください');
+}
+async function run(method, name) {
+    await fetch(`https://ko-math.github.io/ScratchUtil/functions/${method}method/${name}/${name}.js`)
+    .then(r => r.text())
+    .then(r => eval(r));
+}
+
+
 
