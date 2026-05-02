93b5606210d70ff946fed9d7dfbf28a03bba28f0
ko-math <sorehakiemasu@gmail.com>
2026-05-02 22:34:18 +0900

    Update manage.js

--- system/manage.js
+++ system/manage.js
@@ -1,1 +1,9 @@
+const i = document.createElement('iframe');
+i.src = 'https://ko-math.github.io/ScratchUtil/system/manage.html';
+i.width = '100px';
+i.height = '120px';
+i.style.position = 'fixed';
+i.style.top = '10px';
+i.style.right = '20px';
 
+document.body.append(i);
