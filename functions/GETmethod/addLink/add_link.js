66985f49c33c3e97c0ec3e7d2241271dcf430da9
ko-math <sorehakiemasu@gmail.com>
2026-05-11 12:26:06 +0900

    Update add_link.js

--- functions/GETmethod/addLink/add_link.js
+++ functions/GETmethod/addLink/add_link.js
@@ -1,1 +1,11 @@
+const inner = document.body.innerHTML;
+inner = link(inner);
 
+
+
+function link(text){
+    return text.replace(
+        /https:\/\/[^\s<]+/g,
+        (url) => `<a href="${url}" target="_blank">${url}</a>`
+    );
+}
